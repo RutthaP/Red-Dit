d58eb52329ecd2f7a1b47b5d11a2f5b190569e2b
Ruttharakan Pathmanathan <ruttha96@gmail.com>
2020-09-03 20:30:10 +0200

    Component for loading in home page

---
+++ ServletLearning/src/com/login/HomeServlet.java
@@ -0,0 +1,28 @@
+package com.login;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.login.dao.PostDao;
+import com.login.model.Post;
+
+@WebServlet("/home")
+public class HomeServlet extends HttpServlet {
+	PostDao postDao = new PostDao();
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		
+		List<Post> allPosts = postDao.getAllPosts();
+		request.setAttribute("posts", allPosts);
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+		dispatcher.forward(request, response);
+	}
+}
