73c9dc7c2e53e3ab7dcefd1a5a03de56e15bdc24
Ruttharakan Pathmanathan <ruttha96@gmail.com>
2020-09-03 20:36:15 +0200

    Post model and DAO addon

---
+++ ServletLearning/src/com/login/dao/PostDao.java
@@ -0,0 +1,140 @@
+package com.login.dao;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.login.model.Post;
+
+public class PostDao {
+	DBConnection db;
+	
+	public PostDao() {
+		db = new DBConnection("jdbc:postgresql://localhost:5432/fjesbok", "postgres", "pass");
+	}
+	
+	public boolean addPost(int userID, String heading, String content) {
+		String query = "insert into posts(user_id, heading, content, date)"
+				+ " values(?, ?, ?, now())";
+		
+		try {
+			db.establishConnection();
+			db.preparedStatement = db.connection.prepareStatement(query);
+			db.preparedStatement.setInt(1, userID);
+			db.preparedStatement.setString(2, heading);
+			db.preparedStatement.setString(3, content);
+			int insertedRows = db.preparedStatement.executeUpdate();
+			
+			if(insertedRows == 0) {
+				return false;
+			}
+			
+			return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+		finally {
+			db.closeConnection();
+		}
+	}
+	
+	
+	public boolean updatePost(int userID, String heading, String content) {
+		String query = "update posts "
+				+ "set heading = ?, content = ? where user_id = ?, update_date = now()";
+		try {
+			db.establishConnection();
+			db.preparedStatement = db.connection.prepareStatement(query);
+			db.preparedStatement.setString(1, heading);
+			db.preparedStatement.setString(2, content);
+			db.preparedStatement.setInt(3, userID);
+			
+			int updatedRows = db.preparedStatement.executeUpdate();
+			if(updatedRows == 0) {
+				return false;
+			}
+			return true;
+					
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+		finally {
+			db.closeConnection();
+		}
+	}
+	
+	
+	public List<Post> getAllPosts() {
+		String query = "select * from posts order by date desc";
+		
+		try {
+			db.establishConnection();
+			db.statement = db.connection.createStatement();
+			db.resultSet = db.statement.executeQuery(query);
+			
+			if(db.resultSet.isBeforeFirst() == false) {
+				return null;
+			}
+			
+			List<Post> result = new ArrayList<Post>();
+			while(db.resultSet.next()) {
+				Post post = new Post();
+				post.setId(db.resultSet.getInt("id"));
+				post.setUserID(db.resultSet.getInt("user_id"));
+				post.setHeading(db.resultSet.getString("heading"));
+				post.setDate(db.resultSet.getTimestamp("date"));
+				if(db.resultSet.getString("content") != null) {
+					post.setContent(db.resultSet.getString("content"));
+				}
+				if(db.resultSet.getTimestamp("update_date") != null) {
+					post.setUpdateDate(db.resultSet.getTimestamp("update_date"));
+				}
+				result.add(post);
+			}
+			
+			return result;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	
+	public List<Post> getUserPosts(int userID){
+		String query = "select * from posts where user_id=" + userID + "order by date desc";
+		
+		db.establishConnection();
+		try {
+			db.statement = db.connection.createStatement();
+			db.resultSet = db.statement.executeQuery(query);
+			
+			if(db.resultSet.isBeforeFirst() == false) {
+				return null;
+			}
+			
+			List<Post> result = new ArrayList<Post>();
+			while(db.resultSet.next()) {
+				Post post = new Post();
+				post.setId(db.resultSet.getInt("id"));
+				post.setUserID(userID);
+				post.setHeading(db.resultSet.getString("heading"));
+				post.setDate(db.resultSet.getTimestamp("date"));
+				if(db.resultSet.getString("content") != null)
+					post.setContent(db.resultSet.getString("content"));
+				if(db.resultSet.getTimestamp("update_date") != null)
+					post.setUpdateDate(db.resultSet.getTimestamp("update_date"));
+				
+				result.add(post);
+			}
+			
+			return result;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
---
+++ ServletLearning/src/com/login/model/Post.java
@@ -0,0 +1,49 @@
+package com.login.model;
+
+import java.sql.Timestamp;
+
+public class Post {
+	int id, userID;
+	String heading, content;
+	Timestamp date, updateDate;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public int getUserID() {
+		return userID;
+	}
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+	public String getHeading() {
+		return heading;
+	}
+	public void setHeading(String heading) {
+		this.heading = heading;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Timestamp getDate() {
+		return date;
+	}
+	public void setDate(Timestamp date) {
+		this.date = date;
+	}
+	public Timestamp getUpdateDate() {
+		return updateDate;
+	}
+	public void setUpdateDate(Timestamp updateDate) {
+		this.updateDate = updateDate;
+	}
+	
+	
+}
