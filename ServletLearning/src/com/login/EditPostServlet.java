6ae9e6dc0637179ca380c896b6c8a7624dc6f923
Ruttharakan Pathmanathan <ruttha96@gmail.com>
2020-09-05 20:05:51 +0200

    new servlet that updates an existing post using postdao

---
+++ ServletLearning/src/com/login/EditPostServlet.java
@@ -0,0 +1,29 @@
+package com.login;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.login.dao.PostDao;
+
+@WebServlet("/editPost")
+public class EditPostServlet extends HttpServlet {
+	PostDao postDao = null;
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String heading = request.getParameter("heading");
+		String content = request.getParameter("content");
+		HttpSession session = request.getSession();
+		String username = session.getAttribute("username").toString();
+		postDao = new PostDao();
+		
+		postDao.updatePost(username, heading, content);
+		
+		response.sendRedirect("home");
+	}
+}
